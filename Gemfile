33f8de2b70d4c4db3aec1c300d6001c0b8f4cc66
Eric Roberts <ericroberts@gmail.com>
2016-08-09 11:30:47 -0400

    Run code coverage tool when running tests

--- Gemfile
+++ Gemfile
@@ -24,5 +24,6 @@ group :development do
 end
 
 group :test do
+	gem "codeclimate-test-reporter", require: nil
 	gem "rake"
 end
--- test/file_test.rb
+++ test/file_test.rb
@@ -1,4 +1,4 @@
-require "minitest/autorun"
+require_relative "test_helper"
 require_relative "../app/site"
 require_relative "../app/file"
 
