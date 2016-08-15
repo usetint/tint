a3f0fa1aa5e1b03519114bab0267ddcbe115b6e5
Eric Roberts <ericroberts@gmail.com>
2016-08-15 14:39:45 -0400

    Remove duplicated test code

--- test/directory_test.rb
+++ test/directory_test.rb
@@ -3,14 +3,7 @@ require_relative "../app/site"
 require_relative "../app/directory"
 
 describe Tint::Directory do
-	let(:site) do
-		Tint::Site.new(
-			site_id: 1,
-			user_id: 1,
-			cache_path: Pathname.new(__FILE__).dirname.join("data"),
-			fn: "Test Site"
-		)
-	end
+	let(:site) { test_site }
 	let(:path) { "directory" }
 	let(:subject) { Tint::Directory.new(site, path) }
 
