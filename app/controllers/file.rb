9b4f9c1e5650cab2c05a43f62e4d04e7a6888ad1
Eric Roberts <ericroberts@gmail.com>
2016-08-16 13:59:44 -0400

    Break apart put and post methods into different blocks.

--- app/controllers/file.rb
+++ app/controllers/file.rb
@@ -29,6 +29,22 @@ module Tint
 				condition { resource.yml? || !resource.content? }
 			end
 
+			def self.rename(_)
+				condition { !!params[:name] }
+			end
+
+			def self.source(_)
+				condition { !!params[:source] }
+			end
+
+			def self.upload(_)
+				condition { !!params[:file] }
+			end
+
+			def self.new_folder(_)
+				condition { !!params[:folder] }
+			end
+
 			namespace "/:site/files" do
 				get "/?*", query: "download" do
 					if resource.exist? && resource.file?
@@ -97,46 +113,62 @@ module Tint
 					end
 				end
 
+				put "/*", rename: true do
+					authorize resource, :update?
+
+					new = resource.parent.resource(params[:name])
+					if new.exist?
+						slim :error, locals: { message: "A file with that name already exists" }
+					else
+						site.commit_with("Renamed #{resource.relative_path} to #{new.name}", pundit_user) do |dir|
+							dir.join(resource.relative_path).rename(dir.join(new.relative_path))
+						end
+
+						redirect to(resource.parent.route)
+					end
+				end
+
+				put "/*", source: true do
+					authorize resource, :update?
+
+					site.commit_with("Modified #{resource.relative_path}", pundit_user) do |dir|
+						dir.join(resource.relative_path).write params[:source].encode(universal_newline: true)
+					end
+
+					redirect to(resource.parent.route)
+				end
+
+				put "/*", upload: true do
+					authorize resource, :update?
+
+					if params[:file].is_a?(Hash) && params[:file][:tempfile]
+						site.commit_with("Modified #{resource.relative_path}") do |dir|
+							FormHelpers.upload(dir.join(resource.parent.relative_path), params[:file], resource.name)
+						end
+					end
+
+					redirect to(resource.parent.route)
+				end
+
 				put "/*" do
 					authorize resource, :update?
 
-					if params[:name]
-						new = resource.parent.resource(params[:name])
-						if new.exist?
-							return slim :error, locals: { message: "A file with that name already exists" }
-						else
-							site.commit_with("Renamed #{resource.relative_path} to #{new.name}", pundit_user) do |dir|
-								dir.join(resource.relative_path).rename(dir.join(new.relative_path))
-							end
-						end
-					elsif params[:source]
-						site.commit_with("Modified #{resource.relative_path}", pundit_user) do |dir|
-							dir.join(resource.relative_path).write params[:source].encode(universal_newline: true)
-						end
-					elsif params[:file]
-						if params[:file].is_a?(Hash) && params[:file][:tempfile]
-							site.commit_with("Modified #{resource.relative_path}") do |dir|
-								FormHelpers.upload(dir.join(resource.parent.relative_path), params[:file], resource.name)
-							end
-						end
-					else
-						site.commit_with("Modified #{resource.relative_path}") do |dir|
-							updated_data = FormHelpers.process(params[:data], dir)
-							dir.join(resource.relative_path).open("w") do |f|
-								if updated_data
-									if resource.yml?
-										f.puts updated_data.to_yaml.sub(/\A---\r?\n?/, "")
-									else
-										f.puts updated_data.to_yaml
-										f.puts "---"
-									end
+					site.commit_with("Modified #{resource.relative_path}") do |dir|
+						updated_data = FormHelpers.process(params[:data], dir)
+						dir.join(resource.relative_path).open("w") do |f|
+							if updated_data
+								if resource.yml?
+									f.puts updated_data.to_yaml.sub(/\A---\r?\n?/, "")
+								else
+									f.puts updated_data.to_yaml
+									f.puts "---"
 								end
+							end
 
-								if params.has_key?(:content)
-									f.puts(params[:content].encode(universal_newline: true))
-								elsif !resource.yml?
-									resource.stream_content(&f.method(:puts))
-								end
+							if params.has_key?(:content)
+								f.puts(params[:content].encode(universal_newline: true))
+							elsif !resource.yml?
+								resource.stream_content(&f.method(:puts))
 							end
 						end
 					end
@@ -144,18 +176,21 @@ module Tint
 					redirect to(resource.parent.route)
 				end
 
-				post "/?*" do
+				post "/?*", upload: true do
 					authorize resource, :update?
 
-					if params[:file]
-						site.commit_with("Uploaded #{resource.relative_path.join(params[:file][:filename])}") do |dir|
-							FormHelpers.upload(dir.join(resource.relative_path), params[:file])
-						end
-						redirect to(resource.route)
-					elsif params[:folder]
-						new_folder = Tint::Directory.new(site, resource.relative_path.join(params[:folder]))
-						redirect to(new_folder.route)
+					site.commit_with("Uploaded #{resource.relative_path.join(params[:file][:filename])}") do |dir|
+						FormHelpers.upload(dir.join(resource.relative_path), params[:file])
 					end
+
+					redirect to(resource.route)
+				end
+
+				post "/?*", new_folder: true do
+					authorize resource, :update?
+
+					new_folder = Tint::Directory.new(site, resource.relative_path.join(params[:folder]))
+					redirect to(new_folder.route)
 				end
 
 				delete "/*" do
