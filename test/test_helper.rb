c30d2cec63ca52591969a6fcf6aea745bffde487
Eric Roberts <ericroberts@gmail.com>
2016-08-17 11:25:23 -0400

    Set a session secret even in test so that we don't get warnings

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,11 @@ CodeClimate::TestReporter.start
 require "minitest/autorun"
 require "rack/test"
 require "pry"
+require "securerandom"
 
 require_relative "db"
+
+ENV["SESSION_SECRET"] = SecureRandom.uuid
 
 def assert_method_called_on_member(subject, member, method, args=[])
 	mock = MiniTest::Mock.new
